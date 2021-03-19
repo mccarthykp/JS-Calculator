91666c424d15beed3b6717937f7f42e0b0ec2690
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-03-19 11:31:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// add js to give calculator functionality
-
 var firstpart = 0
 var secondpart = 0
 var op = 0
@@ -7,14 +5,13 @@ var dot = 0
 
 function numbers(value){
 
-    //wherever this functon is called, get the value of what is on the screen
+    // wherever this functon is called, get the value of what is on the screen
     var screen=document.getElementById("screen");
 
-    //I used top down approach to code this, read from the elseif statement to enable easy understanding  
-    //check if the = sign has been pressed. the variable value is where anything that is pressed is stored. it is passed through the function number()
+    // check if = has been pressed. variable value is where anything pressed is stored. it is passed through the function number()
     if(value=="=")
     {
-        //split the input using the split function, which returns it as an array 
+        // split input using split function, which returns it as an array 
         var all=screen.value.split(" ")
         firstpart=parseFloat(all[0]);
         op=all[1];
@@ -57,31 +54,31 @@ function numbers(value){
         }
     }
 
-    //if the equals sign wasnt pressed,then the following will be evaluated.
-    // checks for what is on the screen and what is pressd
+    // if the equals sign wasn't pressed, the following will be evaluated.
+    // checks what is on screen and what is pressed
     else if(screen.value =="0" && value=="0")
     {
         return null;
     }
 
-    //it checks for what is on the screen and what is pressed
+    // checks what is on the screen and what is pressed
     else if(screen.value=="0" && !((value=="*" || value=="/" )|| ((value =="+"|| value=="-")|| (value=="%"))))
     {
         screen.value=value;
         secondpart+=value;
     }
-    //if actually a number was pressed, it will save it on an already decleared variable and on the screen
+    // if a number was pressed, it will save it on already decleared variable and on screen
     else if(!((value=="*" || value=="/" )||((value =="+"|| value=="-")|| (value=="%"))))
     {
         screen.value+=value;
         secondpart+=screen.value;
     }
 
-    //when eventually an operation is pressed,it will build this string as say 2  *  2 with spaces in between to help me split the array
+    // when an operation is pressed, it will build the string wuth spaces to help split the array
     else if (op=="0" && ((value=="*" || value=="/" )|| ((value =="+"|| value=="-")|| (value=="%"))))
     {
     
-    //building the string
+    // building the string
     screen.value+=" ";
     screen.value+=value;
     screen.value+=" ";
@@ -109,12 +106,12 @@ function dots(dt){
     var screen=document.getElementById("screen");
     screen.value+=dt;
 
-    // i changed the value of dot to avoid pressing it two times 
+    // changes the value of dot to avoid pressing it two times 
     dot="recoded"
     }
     else
     {
-    //returns an empty stuff if you have pressed dot for the first time
+    // returns empty if you pressed dot for first time
     return;
     }
 }
